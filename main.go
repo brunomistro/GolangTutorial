18344abe4c0ce67d3fa43dffe8b9ef42534121c1
Bruno Mistro <brunomistro@gmail.com>
2021-05-24 14:19:48 -0300

    Day 4

--- main.go
+++ main.go
@@ -2,36 +2,47 @@ package main
 
 import "fmt"
 
-//Printing & Formatting Strings
+//Arrays and Slices
 
 func main() {
-	age := 22
-	name := "Bruno"
 
-	// Print
-	fmt.Print("hello, ")
-	fmt.Print("world! \n")
-	fmt.Print("new line \n")
-
-	// Println
-	fmt.Println("hello ninjas!")
-	fmt.Println("goodbye ninjas!")
-	fmt.Println("my age is", age, "and my name is", name)
+	// var ages [3]int = [3]int{20, 25, 30}
 	/*
-		So, printing works pretty much as expected in print line and just print, \n works 2
-		For getting variables and strings on a print, just separate and call the variables
+		Both declaring are the same, but it's easier to do 2nd version (below),
+		'cause de 'int' after the '=' is mandatory,
+		so no need to give it a type b4.
+		In Go array are fixed length.
+		START WITH 0!
+	*/
+	var ages = [3]int{20, 25, 30}
+
+	names := [4]string{"yoshi", "mario", "peach", "bowser"} //short declare
+	names[1] = "luigi"                                      //updating
+
+	fmt.Println(ages, len(ages)) //'len()' length function
+	fmt.Println(names, len(names))
+
+	// slices (use arrays under the hood)
+	// and can be added values to that slice
+	// NO NEEDED AMOUNT ON THE BRACKETS
+	var scores = []int{100, 50, 60}
+	scores[2] = 25 // updating
+	scores = append(scores, 85)
+	/* So, just using 'append()', it's going to return the scores slice whit a value, 85 in this case,
+	then for adding a value to the slice, u need to equals to the slice, as above.
 	*/
 
-	// Printf (formatted string), %_ = format specifier
-	// always add \n !!!!
-	fmt.Printf("my age is %v and my name is %v \n", age, name) // %v = value in default format
-	fmt.Printf("my age is %q and my name is %q \n", age, name) // %q = quotes
-	fmt.Printf("age is of type %T \n", age)                    // %T is the type
-	fmt.Printf("you scored %f points! \n", 225.55)             // %f = float format
-	fmt.Printf("you scored %0.1f points! \n", 225.55)          // %0.2f = float with 2 decimal points
+	fmt.Println(scores, len(scores))
 
-	// Sprintf (save formatted strings)
-	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-	fmt.Println("the saved string is:", str)
-	//don't know why use this if can just print directly, but exists
+	// slice ranges , kinda substrings ?? dk
+	// wierd AF
+	rangeOne := names[1:4]  // doesn't include pos 4 element
+	rangeTwo := names[2:]   //includes the last element
+	rangeThree := names[:3] //does not includes the pos 3 element
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+	fmt.Printf("the type of rangeOne is %T \n", rangeOne)
+
+	rangeOne = append(rangeOne, "koopa")
+	fmt.Println(rangeOne)
+
 }
