9c292c631df5494b13a039d4aef2c4d92f486cdc
Bruno Mistro <brunomistro@gmail.com>
2021-05-26 14:57:06 -0300

    Day 5

--- main.go
+++ main.go
@@ -1,48 +1,121 @@
 package main
 
-import "fmt"
-
-//Arrays and Slices
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
+	//The Standard Library - Part 1
+	part1()
+	//Loops - Part 2
+	part2()
+	//Booleans & Conditionals - Part 3
+	part3()
+}
 
-	// var ages [3]int = [3]int{20, 25, 30}
-	/*
-		Both declaring are the same, but it's easier to do 2nd version (below),
-		'cause de 'int' after the '=' is mandatory,
-		so no need to give it a type b4.
-		In Go array are fixed length.
-		START WITH 0!
-	*/
-	var ages = [3]int{20, 25, 30}
+func part1() {
+	fmt.Println("Begining of The Standard Library - Part 1")
 
-	names := [4]string{"yoshi", "mario", "peach", "bowser"} //short declare
-	names[1] = "luigi"                                      //updating
+	greeting := "hello there friends!"
 
-	fmt.Println(ages, len(ages)) //'len()' length function
-	fmt.Println(names, len(names))
+	fmt.Println(strings.Contains(greeting, "hello"))
+	fmt.Println(strings.Contains(greeting, "bye"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "ll"))
+	fmt.Println(strings.Split(greeting, " "))
 
-	// slices (use arrays under the hood)
-	// and can be added values to that slice
-	// NO NEEDED AMOUNT ON THE BRACKETS
-	var scores = []int{100, 50, 60}
-	scores[2] = 25 // updating
-	scores = append(scores, 85)
-	/* So, just using 'append()', it's going to return the scores slice whit a value, 85 in this case,
-	then for adding a value to the slice, u need to equals to the slice, as above.
-	*/
+	// the original value is unchanged
+	fmt.Println("original string value =", greeting)
 
-	fmt.Println(scores, len(scores))
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-	// slice ranges , kinda substrings ?? dk
-	// wierd AF
-	rangeOne := names[1:4]  // doesn't include pos 4 element
-	rangeTwo := names[2:]   //includes the last element
-	rangeThree := names[:3] //does not includes the pos 3 element
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
-	fmt.Printf("the type of rangeOne is %T \n", rangeOne)
+	sort.Ints(ages)
+	fmt.Println(ages)
 
-	rangeOne = append(rangeOne, "koopa")
-	fmt.Println(rangeOne)
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
 
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	sort.Strings(names)
+	fmt.Println(names)
+	fmt.Println(sort.SearchStrings(names, "bowser"))
+
+	fmt.Println("## End of The Standard Library - Part 1 ##")
+	fmt.Println("")
+}
+
+func part2() {
+	fmt.Println("Begining of Loops - Part 2")
+
+	x := 0
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++ // infinite loop without this
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i)
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	for index, val := range names {
+		fmt.Printf("the value at pos %v is %v \n", index, val)
+		val = "new string"
+	}
+
+	for _, val := range names {
+		fmt.Print(val, ",")
+		val = "new string"
+	}
+
+	// changing val in a loop does not mutate the original slice
+	fmt.Println(names)
+
+	fmt.Println("## End of Loops - Part 2 ##")
+	fmt.Println("")
+}
+
+func part3() {
+	fmt.Println("Begining of Booleans & Conditionals - Part 3")
+
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, val := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, val)
+	}
+
+	fmt.Println("## End of Booleans & Conditionals - Part 3 ##")
+	fmt.Println("")
 }
