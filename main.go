838e6963d8d306a1f8bab90d43f8fdeb973a0dd1
Bruno Mistro <brunomistro@gmail.com>
2021-05-27 09:42:09 -0300

    Day 6

--- main.go
+++ main.go
@@ -2,120 +2,40 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
+	"math"
 )
 
+//Using Functions
+
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+
+func cycleNames(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
+
 func main() {
-	//The Standard Library - Part 1
-	part1()
-	//Loops - Part 2
-	part2()
-	//Booleans & Conditionals - Part 3
-	part3()
-}
+	sayGreeting("mario")
+	sayGreeting("luigi")
+	sayBye("mario")
 
-func part1() {
-	fmt.Println("Begining of The Standard Library - Part 1")
+	ff := []string{"cloud", "barret", "tifa"}
+	cycleNames(ff, sayGreeting)
+	cycleNames(ff, sayBye)
 
-	greeting := "hello there friends!"
-
-	fmt.Println(strings.Contains(greeting, "hello"))
-	fmt.Println(strings.Contains(greeting, "bye"))
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "ll"))
-	fmt.Println(strings.Split(greeting, " "))
-
-	// the original value is unchanged
-	fmt.Println("original string value =", greeting)
-
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
-
-	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
-
-	sort.Strings(names)
-	fmt.Println(names)
-	fmt.Println(sort.SearchStrings(names, "bowser"))
-
-	fmt.Println("## End of The Standard Library - Part 1 ##")
-	fmt.Println("")
-}
-
-func part2() {
-	fmt.Println("Begining of Loops - Part 2")
-
-	x := 0
-	for x < 5 {
-		fmt.Println("value of x is:", x)
-		x++ // infinite loop without this
-	}
-
-	for i := 0; i < 5; i++ {
-		fmt.Println("value of i is:", i)
-	}
-
-	names := []string{"mario", "luigi", "yoshi", "peach"}
-
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
-	}
-
-	for index, val := range names {
-		fmt.Printf("the value at pos %v is %v \n", index, val)
-		val = "new string"
-	}
-
-	for _, val := range names {
-		fmt.Print(val, ",")
-		val = "new string"
-	}
-
-	// changing val in a loop does not mutate the original slice
-	fmt.Println(names)
-
-	fmt.Println("## End of Loops - Part 2 ##")
-	fmt.Println("")
-}
-
-func part3() {
-	fmt.Println("Begining of Booleans & Conditionals - Part 3")
-
-	age := 45
-
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
-
-	if age < 30 {
-		fmt.Println("age is less than 30")
-	} else if age < 40 {
-		fmt.Println("age is less than 40")
-	} else {
-		fmt.Println("age is not less than 40")
-	}
-
-	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
-
-	for index, val := range names {
-		if index == 1 {
-			fmt.Println("continuing at pos", index)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("breaking at pos", index)
-			break
-		}
-		fmt.Printf("the value at pos %v is %v \n", index, val)
-	}
-
-	fmt.Println("## End of Booleans & Conditionals - Part 3 ##")
-	fmt.Println("")
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+	fmt.Println(a1, a2)
+	fmt.Printf("circle 1 area is %0.3f & circle 2 area is %0.3f \n", a1, a2)
 }
