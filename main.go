080bf89ac18101cb7e4e58fdc0cfab3d30aadeb9
Bruno Mistro <brunomistro@gmail.com>
2021-06-01 14:04:55 -0300

    Day 8

--- main.go
+++ main.go
@@ -1,57 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-//Multiple Return Values - Part 1
-
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-		/*
-			to get the last element of a slice is array[len(array)-1]
-		*/
-	}
-
-	return initials[0], "_"
-}
-
-func multReturnValues() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
-
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
-
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
-}
-
-//Package Scope - Part 2
-
-func packageScope() {
-	sayHello("mario")
-	showScore()
-
-	for _, v := range points {
-		fmt.Println(v)
-	}
-}
-
-var score float64 = 99.5
+//Maps
 
 func main() {
-	//score = 99.5
-	multReturnValues()
-	packageScope()
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		984759373: "luigi",
+		845775485: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[845775485])
+
+	phonebook[984759373] = "bowser"
+	fmt.Println(phonebook)
+
+	phonebook[647583927] = "yoshi"
+	fmt.Println(phonebook)
 }
