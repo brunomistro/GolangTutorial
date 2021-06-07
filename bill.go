382b6a86e003630c7b92a200b0a967bbd390fdac
Bruno Mistro <brunomistro@gmail.com>
2021-06-07 16:19:59 -0300

    Day 12

--- bill.go
+++ bill.go
@@ -29,8 +29,21 @@ func (b bill) format() string {
 		total += v
 	}
 
+	//add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
+
 	// add total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+// add an item to the bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+//Receiver Functions with Pointers
+
 func main() {
 	mybill := newBill("Bruno")
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 8.95)
+	mybill.addItem("toffee pudding", 4.95)
+	mybill.addItem("coffee", 3.25)
+	mybill.updateTip(10)
 	fmt.Println(mybill.format())
 }
