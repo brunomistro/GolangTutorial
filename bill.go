82ef138be140d4a69c55c9282c667f17a0c64f37
Bruno Mistro <brunomistro@gmail.com>
2021-06-10 13:05:38 -0300

    Day 15

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -46,4 +49,14 @@ func (b bill) format() string {
 // update tip
 func (b *bill) updateTip(tip float64) {
 	b.tip = tip
+}
+
+// Save the bill
+func (b *bill) save() {
+	data := []byte(b.format())
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill saved to file")
 }
--- main.go
+++ main.go
@@ -8,13 +8,7 @@ import (
 	"strings"
 )
 
-// Parsing Floats
-
-// func getInput(prompt string, r *bufio.Reader) (string, error) {
-// 	fmt.Print(prompt)
-// 	input, err := r.ReadString('\n')
-// 	return strings.TrimSpace(input), err
-// }
+// Saving Files
 
 func getInput(prompt string) (string, error) {
 	reader := bufio.NewReader(os.Stdin)
@@ -24,13 +18,6 @@ func getInput(prompt string) (string, error) {
 }
 
 func createBill() bill {
-	//reader := bufio.NewReader(os.Stdin)
-	/*
-		Trying to do the reader on func, works properly,
-		I'll let you know in future branches,
-		if that has any problems
-		(The video func above)
-	*/
 	name, _ := getInput("Create a new bill name: ")
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
@@ -43,32 +30,29 @@ func promptOptions(b bill) {
 	case "a":
 		name, _ := getInput("Item name: ")
 		price, _ := getInput("Item price: ")
-
 		p, err := strconv.ParseFloat(price, 64)
 		if err != nil {
 			fmt.Println("The price must be a number!")
 			promptOptions(b)
 		}
 		b.addItem(name, p)
-
 		fmt.Println("Item added - ", name, price)
 		promptOptions(b)
 
 	case "t":
 		tip, _ := getInput("Enter tip amount ($): ")
-
 		t, err := strconv.ParseFloat(tip, 64)
 		if err != nil {
 			fmt.Println("The tip must be a number!")
 			promptOptions(b)
 		}
 		b.updateTip(t)
-
 		fmt.Println("Tip added - ", tip)
 		promptOptions(b)
 
 	case "s":
-		fmt.Println("you chose to save the bill", b)
+		b.save()
+		fmt.Println("Bill has been saved as: ", b.name)
 
 	default:
 		fmt.Println("That was not a valid option...")
